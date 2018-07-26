d67d4a7174fd95cc83a7502b075fa380a78d7b01
Abimbola21 <bimzy4me@gmail.com>
2018-07-26 11:10:23 -0500

    More Changes

--- MechanicsForum/Models/User.Context.cs
+++ MechanicsForum/Models/User.Context.cs
@@ -25,13 +25,15 @@ namespace MechanicsForum.Models
             throw new UnintentionalCodeFirstException();
         }
     
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
         public virtual DbSet<Answer> Answers { get; set; }
+        public virtual DbSet<AnswersMedia> AnswersMedias { get; set; }
         public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
-        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
         public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
         public virtual DbSet<Problem> Problems { get; set; }
-        public virtual DbSet<ProblemsMedia> ProblemsMedia { get; set; }
-        public virtual DbSet<AnswersMedia> AnswersMedia { get; set; }
+        public virtual DbSet<ProblemsMedia> ProblemsMedias { get; set; }
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
     }
 }
